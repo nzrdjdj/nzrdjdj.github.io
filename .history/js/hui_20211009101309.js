deef757289cf126a280030fb1341998379bbb79f
nzrdjdj <2019249563@qq.com>
2023-06-26 17:07:52 +0800

    上传全部文件

---
+++ .history/js/hui_20211009101309.js
@@ -0,0 +1,31 @@
+var nei = document.getElementById("lonzun").children;
+// var left = document.getElementsByClassName("left")[0];
+var lunbo = document.getElementsByClassName("lunbox")[0];
+// var right = document.getElementsByClassName("right")[0];
+var bu = document.getElementById("bu").children;
+var index = 0
+function str() {
+    index++
+    if (index >= 6) {
+        index = 0;
+    }
+    main()
+}
+
+function main() {
+    for (var i = 0; i < nei.length; i++) {
+        nei[i].style.display = "none"
+        bu[i].classList.remove("hei")
+    }
+    nei[index].style.display = "block"
+    bu[index].classList.add("hei")
+}
+
+item = setInterval(str, 2000)
+// 自动轮播
+lunbox.onmouseover = function () {
+    clearInterval(item)
+}
+lunbox.onmouseout = function () {
+    item = setInterval(str, 2000)
+}
